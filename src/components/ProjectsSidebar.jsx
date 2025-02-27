2b21e8b6e2faa6f0dfc391a85607cf56803d78b9
xhyasar <hazal-hazal-yasar@hotmail.com>
2025-02-27 18:28:22 +0300

    Integrate Tailwind CSS and add ProjectsSidebar component

---
+++ src/components/ProjectsSidebar.jsx
@@ -0,0 +1,11 @@
+export default function ProjectsSidebar() {
+    return (
+        <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
+            <h2>Your Projects</h2>
+            <div>
+                <button>+ Add Projects</button>
+            </div>
+            <ul></ul>
+        </aside>
+    );
+}

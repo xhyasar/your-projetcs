7a39a3a01e4ea28c84561f9e7302f65121adae5d
xhyasar <hazal-hazal-yasar@hotmail.com>
2025-02-28 13:38:32 +0300

    Add NoProjectSelected component and Button; update App layout and ProjectsSidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import ProjectsSidebar from "./components/ProjectsSidebar.jsx";
 import NewProjects from "./components/NewProjects.jsx";
+import NoProjectSelected from "./components/NoProjectSelected.jsx";
 
 function App() {
     return (
         <main className="h-screen my-8 flex gap-8">
             <ProjectsSidebar/>
-            <NewProjects/>
+            <NoProjectSelected/>
         </main>
     );
 }

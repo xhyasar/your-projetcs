0a9b0a49d3c2cc6e94019f3731337476e6dd9030
xhyasar <hazal-hazal-yasar@hotmail.com>
2025-02-28 21:12:31 +0300

    Refactor Input component to remove forwardRef and simplify implementation

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,5 +1,3 @@
-import {forwardRef} from "react";
-
 export default function Input({label, textarea, ...props}) {
     const classes = "w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600";
     return (

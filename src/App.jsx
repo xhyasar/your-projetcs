993f12ede3cc23d0c5572932de9eadd5e8fe0b47
xhyasar <hazal-hazal-yasar@hotmail.com>
2025-02-28 13:17:30 +0300

    Add NewProjects component and update App layout; enhance Input styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import ProjectsSidebar from "./components/ProjectsSidebar.jsx";
+import NewProjects from "./components/NewProjects.jsx";
 
 function App() {
-  return (
-    <main className="h-screen my-8">
-      <ProjectsSidebar />
-    </main>
-  );
+    return (
+        <main className="h-screen my-8 flex gap-8">
+            <ProjectsSidebar/>
+            <NewProjects/>
+        </main>
+    );
 }
 
 export default App;

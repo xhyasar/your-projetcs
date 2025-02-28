5c8ca6219c6304123bb72a0a562068848a2407cf
xhyasar <hazal-hazal-yasar@hotmail.com>
2025-02-28 16:54:14 +0300

    Add project listing in ProjectsSidebar; pass projects as props and render project titles

--- src/App.jsx
+++ src/App.jsx
@@ -20,30 +20,35 @@ function App() {
 
     function handleAddProject(projectData) {
         setProjectsState(prevState => {
+            const projectId = Math.random();
             const newProject = {
                 ...projectData,
-                id:Math.random()
+                id: projectId,
             };
 
             return {
                 ...prevState,
+                selectedProjectId: undefined,
                 projects: [...prevState.projects, newProject]
             };
         });
     }
 
-console.log(projectsState);
+
     let content;
 
     if (projectsState.selectedProjectId === null) {
-        content = <NewProjects onAdd={handleAddProject} />
+        content = <NewProjects onAdd={handleAddProject}/>
     } else if (projectsState.selectedProjectId === undefined) {
-        content = <NoProjectSelected onStartAddProject={handleStartAddProject} />
+        content = <NoProjectSelected onStartAddProject={handleStartAddProject}/>
     }
 
     return (
         <main className="h-screen my-8 flex gap-8">
-            <ProjectsSidebar onStartAddProject={handleStartAddProject}/>
+            <ProjectsSidebar
+                onStartAddProject={handleStartAddProject}
+                projects={projectsState.projects}
+            />
             {content}
         </main>
     );

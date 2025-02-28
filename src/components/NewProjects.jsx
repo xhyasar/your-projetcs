34e9072ff0b9f8d39fecf75a1e4239beaf2d21df
xhyasar <hazal-hazal-yasar@hotmail.com>
2025-02-28 19:46:13 +0300

    Add Modal component and integrate input validation in NewProjects; display error message for invalid inputs

--- src/components/NewProjects.jsx
+++ src/components/NewProjects.jsx
@@ -1,7 +1,10 @@
 import Input from './Input.jsx';
 import {useRef} from "react";
+import Modal from "./Modal.jsx";
 
 export default function NewProjects({onAdd}) {
+    const modal = useRef();
+
     const title = useRef();
     const description = useRef();
     const dueDate = useRef();
@@ -11,6 +14,15 @@ export default function NewProjects({onAdd}) {
         const enteredDescription = description.current.value;
         const enteredDueDate = dueDate.current.value;
 
+        if (
+            enteredTitle.trim() === '' ||
+            enteredDescription.trim() === '' ||
+            enteredDueDate.trim() === ''
+        ) {
+            modal.current.open();
+            return;
+        }
+
         onAdd({
             title: enteredTitle,
             description: enteredDescription,
@@ -19,22 +31,29 @@ export default function NewProjects({onAdd}) {
     }
 
     return (
-        <div className="w-[35rem] mt-16">
-            <menu className="flex items-center justify-end gap-4 my-4">
-                <li>
-                    <button className="text-stone-800 hover:text-stone-950">Cancel</button>
-                </li>
-                <li>
-                    <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
-                            onClick={handleSave}>Save
-                    </button>
-                </li>
-            </menu>
-            <div>
-                <Input type="text" ref={title} label="Title"/>
-                <Input ref={description} label="Description" textarea/>
-                <Input type="date" ref={dueDate} label="Due Date"/>
+        <>
+            <Modal ref={modal} buttonCaption="Okay">
+                <h2>Invalid Input</h2>
+                <p>Oops ... looks like you forgot to enter a value.</p>
+                <p>Please make sure you provide a valid value for every input field.</p>
+            </Modal>
+            <div className="w-[35rem] mt-16">
+                <menu className="flex items-center justify-end gap-4 my-4">
+                    <li>
+                        <button className="text-stone-800 hover:text-stone-950">Cancel</button>
+                    </li>
+                    <li>
+                        <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+                                onClick={handleSave}>Save
+                        </button>
+                    </li>
+                </menu>
+                <div>
+                    <Input type="text" ref={title} label="Title"/>
+                    <Input ref={description} label="Description" textarea/>
+                    <Input type="date" ref={dueDate} label="Due Date"/>
+                </div>
             </div>
-        </div>
+        </>
     )
 }
--- src/components/NoProjectSelected.jsx
+++ src/components/NoProjectSelected.jsx
@@ -2,7 +2,7 @@ import noProjectImage from '../assets/no-projects.png';
 import Button from "./Button.jsx";
 
 export default function NoProjectSelected({onStartAddProject}) {
-    return <div className="mt-24 text-center w-2/3">/
+    return <div className="mt-24 text-center w-2/3">
         <img
             src={noProjectImage}
             alt="An empty task list"

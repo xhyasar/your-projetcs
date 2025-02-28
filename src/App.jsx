b4df9661a043b36038e454593b89874f3f899364
xhyasar <hazal-hazal-yasar@hotmail.com>
2025-02-28 14:09:26 +0300

    Add project management functionality; implement state handling and update components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
 import ProjectsSidebar from "./components/ProjectsSidebar.jsx";
 import NewProjects from "./components/NewProjects.jsx";
 import NoProjectSelected from "./components/NoProjectSelected.jsx";
+import {useState} from "react";
 
 function App() {
+    const [projectsState, setProjectsState] = useState({
+        selectedProjectId: undefined,
+        projects: []
+    });
+
+    function handleStartAddProject() {
+        setProjectsState(prevState=> {
+            return {
+                ...prevState,
+                selectedProjectId: null,
+            };
+        });
+    }
+
+    let content;
+
+    if(projectsState.selectedProjectId === null){
+        content= <NewProjects />
+    } else if(projectsState.selectedProjectId === undefined){
+        content= <NoProjectSelected />
+    }
+
     return (
         <main className="h-screen my-8 flex gap-8">
-            <ProjectsSidebar/>
-            <NoProjectSelected/>
+            <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+            {content}
         </main>
     );
 }
